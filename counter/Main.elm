fb3d889b4c0e4b654e586b842247f12856653ac9
Dan Murphy <murphydbuffalo@gmail.com>
2016-09-28 13:11:53 -0400

    Refresher on the Elm getting started guide.
    + Added comments for my own reference/to reinforce concepts introduced
    by the guides.
    
    + Implemented the counter reset functionality described in the guide.

--- counter/Main.elm
+++ counter/Main.elm
@@ -1,18 +1,27 @@
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, div, button, text)
+
+-- Modules namespaced within Html
 import Html.App as App
 import Html.Events exposing (onClick)
 
-main = App.beginnerProgram { model = model, update = update, view = view }
+-- A type declaration saying "the variable `main` is of type `Program`"
+main : Program Never
+main = App.beginnerProgram { model = model , view = view , update = update }
 
--- MODEL
-
+-- An alias type saying the type `Model` is another name for the type `Int`
 type alias Model = Int
+
 model : Model
 model = 0
 
--- UPDATE
+-- A union type saying declaring the type `Msg` as valid for any of values provided
+type Msg = Increment | Decrement | Reset
 
-type Msg = Increment | Decrement
+-- This is the syntax for a type declaration specifying a function, its arguments, and return value
+-- It says "update is a function that takes `Msg` and `Model` types as arguments"
+-- and returns a `Model` type
+-- IMPORTANT: `update` is a special function that is automatically invoked whenever
+-- a new `Msg` type is return (as is the case in our `view` function below)
 update : Msg -> Model -> Model
 update msg model =
   case msg of
@@ -20,14 +29,20 @@ update msg model =
       model + 1
     Decrement ->
       model - 1
+    Reset ->
+      0
 
--- VIEW
-
+-- `view` takes a `Model` and returns `Html`, which itself can return `Msg` values
+-- The `onClick` functions return those `Msg` values and thus invoke `update`.
+-- The `view` function is analogous to React.js's `render` function. It expresses
+-- your UI as a function that is populated with your application's state/data.
+-- Elm will intelligently compute the most efficient way to re-render the UI
+-- whenever the `view` function is called, making Elm apps lickedy split.
 view : Model -> Html Msg
 view model =
   div [] [
-    button [onClick Decrement] [text "-"],
     button [onClick Increment] [text "+"],
-    div [] [text (toString model)]
-
+    button [onClick Decrement] [text "-"],
+    div [] [text (toString model)],
+    button [onClick Reset] [text "Reset!"]
   ]
