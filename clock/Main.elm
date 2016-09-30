5662ea6e9d11f06352014bb06b54ed25ee5b386b
Dan Murphy <murphydbuffalo@gmail.com>
2016-09-30 09:23:39 -0400

    Add a pause button to the clock.
    The pause button calls a `Pause` function which toggles a `paused`
    boolean in the model. If that boolean is set to true, then the `Tick`
    function will no longer update the `time` value of the model.

--- clock/Main.elm
+++ clock/Main.elm
@@ -1,7 +1,8 @@
-import Html exposing (Html)
+import Html exposing (Html, div, button, text)
+import Html.Events exposing (onClick)
 import Html.App as App
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
+import Svg exposing (svg, circle, line)
+import Svg.Attributes exposing (fill, stroke, viewBox, width, x1, x2, y1, y2, r, cx, cy)
 import Time exposing (Time, second)
 
 main : Program Never
@@ -13,36 +14,46 @@ main =
     view = view
   }
 
-type alias Model = Time
-type Msg = Tick Time
+type alias Model = { time : Time, paused : Bool }
+
+type Msg = Tick Time | Pause
 
 init : (Model, Cmd Msg)
-init = (0, Cmd.none)
+init = ({ time = 0, paused = False }, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick time ->
-      (time, Cmd.none)
+      if model.paused then
+        (model, Cmd.none)
+      else
+        ({ model | time = time }, Cmd.none)
+    Pause ->
+      ({ model | paused = not model.paused }, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  -- Every second, get the time in milliseconds
+  -- Every second, get the time in milliseconds and pass it to the Tick function
+  -- Calls to Tick are passed automatically to update
   Time.every second Tick
 
 view : Model -> Html Msg
 view model =
   let
     angle =
-      -- `turns` converts floats to radians. 1.0 is equivalent to 360 degrees
-      -- Time.inMinutes converts milliseconds to minutes represented as a as a float
-      turns (Time.inMinutes model)
+      -- `turns` converts floats to radians. 1.0 is equivalent to 360 degrees and 2pi or 6.28 radians
+      -- Time.inMinutes converts milliseconds to minutes represented as a float
+      turns (Time.inMinutes model.time)
     handX =
       toString (50 + (40 * cos angle))
     handY =
       toString (50 + (40 * sin angle))
   in
-    svg [viewBox "0 0 100 100", width "300px"] [
-      circle [cx "50", cy "50", r "45", fill "#0B79CE"] [],
-      line [x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963"] []
+    div [] [
+      svg [viewBox "0 0 100 100", width "300px"] [
+        circle [cx "50", cy "50", r "45", fill "#0B79CE"] [],
+        line [x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963"] []
+      ],
+      button [onClick Pause] [text "Pause"]
     ]
